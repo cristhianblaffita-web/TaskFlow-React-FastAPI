115134bd7bf49393c62b3e7729bd27652b5d1ecc
cristhianblaffita-web <cristhianblaffita@gmail.com>
2026-01-08 11:46:56 -0500

    Fix: Change git repositorys root and add backend folder

---
+++ frontend/src/hooks/ToggleAside.js
@@ -0,0 +1,12 @@
+import {useState} from "react";
+
+export const useAside = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggleAside = () => {
+    setIsOpen(prev => !prev);
+    
+  };
+  
+  return {isOpen, toggleAside};
+}
