04b904056b21f78a1eeba96123411fad7a5cb608
Cristhian B <cristhianblaffita@gmail.com>
2026-01-12 21:54:04 -0500

    Feat: Add a Login formulary, it is not web-responsive at the time. Also fix some general UI issues.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,26 @@
 import React from "react"
+import "../styles/Login.css"
 
 const Login = () => {
   return (
-    <main>
-      
+    <main className="main-login">
+      <form className="login-form">
+        <div className="form-header">
+          <h2>Log in</h2>
+        </div>
+        <fieldset className="field">
+          <label className="label" for="email">Email</label>
+          <input className="form-input" name="email" type="email" placeholder="Your eamil..." required />
+        </fieldset>
+        <fieldset className="field">
+          <label className="label" for="password">Password</label>
+          <input className="form-input" name="password" type="password"
+          placeholder="Your password..." required/>
+        </fieldset>
+        <div className="button-field">
+          <button className="login-btn primary" type="submit">Login</button>
+        </div>
+      </form> 
     </main>
   )
 }
