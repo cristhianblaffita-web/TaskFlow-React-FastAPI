75d418a19008eeb71a90d02c38d89511b012087e
Cristhian B <cristhianblaffita@gmail.com>
2026-01-31 10:51:41 -0500

    feat: Add protected routes

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -4,9 +4,5 @@ import { AuthContext } from "../features/auth/context/authContext";
 export const useAuth = () => {
   const context = useContext(AuthContext);
   
-  if (!context) {
-    throw new Error("uuseAuth debe usarse dentro del AuthProvider");
-  }
-  
   return context;
 };
